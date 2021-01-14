ed4cb0eba27966b1d97ed86c5f69b8c41d904bee
Samantha Ostrowski <isphinxs@hotmail.com>
2021-01-13 20:37:25 -0500

    commit tree 17566c3ae1aab13d84f532c3abcfedc038210bb7

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 5, "M", "Brown", "Fair", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Rowdy", 12, "F", "White", "Unruly", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Tabitha", 3, "F", "Fawn", "Sweet", false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Sergeant Brown", 10, "F", "Black", "Truthful", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Melissa", 1, "F", "Tortoiseshell", "Blonde", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Grinch", 25, "M", "Gray", "Grumpy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Wendy", 16, "F", "Purple", "Wistful", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 7, "M", "Red", "Angry", true);
